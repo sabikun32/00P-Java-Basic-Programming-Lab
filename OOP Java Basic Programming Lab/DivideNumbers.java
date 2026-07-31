b15116e9a8bdab434f9089953090706929f52522
Sabikun Nahar Ikra <sabikuntasnim2010@gmail.com>
2026-07-31 10:03:08 +0600

    Add files via upload

---
+++ OOP Java Basic Programming Lab/DivideNumbers.java
@@ -0,0 +1,8 @@
+public class DivideNumbers {
+    public static void main(String[] args) {
+        int dividend = 50;
+        int divisor = 3;
+        int result = dividend / divisor;
+        System.out.println(result);
+    }
+}
